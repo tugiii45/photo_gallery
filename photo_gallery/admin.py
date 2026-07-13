b1cfb178bb80ed7d505d7ba344cd701b2fee32f7
Conrad <conradkaranja5@gmail.com>
2026-07-13 10:25:32 +0300

    feat: register profile and photo models in admin

--- photo_gallery/admin.py
+++ photo_gallery/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Profile, Photo
 
-# Register your models here.
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ("user", "bio")
+    search_fields = ("user__username", "user__email")
+
+
+@admin.register(Photo)
+class PhotoAdmin(admin.ModelAdmin):
+    list_display = ("title", "uploaded_by", "created_at")
+    search_fields = ("title", "description", "tags")
+    list_filter = ("created_at",)
