22e051b495869cbf9bac4b922409c3cb862dbd41
Conrad <conradkaranja5@gmail.com>
2026-07-13 11:16:14 +0300

    feat: configure Cloudinary image uploads

--- photo_gallery/urls.py
+++ photo_gallery/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path("register/", views.register, name="register"),
+    path("photo/<int:photo_id>/", views.photo_detail, name="photo_detail"),
     path("login/", views.user_login, name="login"),
     path("logout/", views.user_logout, name="logout"),
     path("", views.home, name="home"),
--- photo_gallery/views.py
+++ photo_gallery/views.py
@@ -3,17 +3,35 @@ from django.contrib.auth import login, logout
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .forms import UserRegistrationForm, UserLoginForm
-
+from .models import *
+from django.shortcuts import render, redirect, get_object_or_404
 
 @login_required
 def home(request):
     """
-    Displays the photo gallery homepage to authenticated users.
+    Displays all photos in the gallery.
     """
+
+    photos = Photo.objects.all().order_by("-created_at")
 
     return render(
         request,
         "photo_gallery/home.html",
+        {"photos": photos},
+    )
+
+@login_required
+def photo_detail(request, photo_id):
+    """
+    Displays detailed information about a selected photo.
+    """
+
+    photo = get_object_or_404(Photo, id=photo_id)
+
+    return render(
+        request,
+        "photo_gallery/photo_detail.html",
+        {"photo": photo},
     )
 
 def register(request):
